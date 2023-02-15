5f0740cd962a5048246d98c2f9ea0c3332da4a64
mehzabin-haque <74411032+mehzabin-haque@users.noreply.github.com>
2023-02-15 11:37:05 +0600

    Update Subject.java

--- DesignPatterns/CreationalPattern/ObeserverDesignPattern/Subject.java
+++ DesignPatterns/CreationalPattern/ObeserverDesignPattern/Subject.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Subject {
+public abstract class Subject {
     private List<Observer> observers = new ArrayList<>();
 
     public void registerObserver(Observer observer) {
@@ -26,4 +26,6 @@ public class Subject {
 
         System.out.println("******************************");
     }
+    
+    
 }

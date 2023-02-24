9d56b1edba7ff5229ae8807565477c7114fbbb76
mehzabin-haque <74411032+mehzabin-haque@users.noreply.github.com>
2023-02-24 20:09:11 +0600

    Update index.php

--- WebTech/PHP/index.php
+++ WebTech/PHP/index.php
@@ -1,6 +1,4 @@
-<!-- <?php
-    require_once('test.php');
-?> -->
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,45 +10,70 @@
 
 
     <link rel="stylesheet" href="test.css">
+    <style>
+        .error {color: red;}
+    </style>
 </head>
 
 <body>
-
-
-    <div>
-        <!-- <?php
-        if(isset($_POST['submit'])){
-            $firstname = $_POST['firstname'];
-            $lastname = $_POST['lastname'];
-            $email = $_POST['email'];
-            $addre = $_POST['add'];
-
-            $sql = "INSERT INTO users (firstname,lastname,email,address) VALUES(?,?,?.?)";
-            $stmtinsert = $db->prepare($sql);
-            $result = $stmtinsert->execute([$firstname, $lastname, $email, $addre]);
-            
-            if($result){
-                echo 'Successfully Saved';
-            }
-            else{
-                echo 'Error while saving';
-            }
-            // echo $firstname . " " . $lastname . " " . $email. " " . $addre;
+    <?php
+     $nameErr = $emailErr = $addrErr = "";
+     $firstname = $lastname = $email = $addr =  "";
+     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["firstname"]) || empty($_POST["lastname"])  ) {
+          $nameErr = "*Name is required";
+        } else {
+          $firstname = test_input($_POST["firstname"]);
+          $lastname = test_input($_POST["lastname"]);
+          // check if name only contains letters and whitespace
+          if (!preg_match("/^[a-zA-Z-' ]*$/",$firstname) || !preg_match("/^[a-zA-Z-' ]*$/",$lastname)) {
+            $nameErr = "*Only letters and white space allowed";
+          }
         }
-    ?> -->
-    </div>
-    <form action="connect.php" method="post">
+        
+        if (empty($_POST["email"])) {
+          $emailErr = "*Email is required";
+        } else {
+          $email = test_input($_POST["email"]);
+          // check if e-mail address is well-formed
+          if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "*Invalid email format";
+          }
+        }
+          
+      
+        if (empty($_POST["add"])) {
+          $addrErr = "*Address is required";
+        } else {
+          $addr = test_input($_POST["add"]);
+        }
+      
+       
+      }
+      
+      function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+     ?>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
         <div class="container">
             <h3> Form </h3>
             <ul class="form-style-1">
                 <li><label>Full Name <span class="required">*</span></label><input type="text" name="firstname" class="field-divided" placeholder="First" /> <input type="text" name="lastname" class="field-divided" placeholder="Last" /></li>
+                <span class="error"> <?php echo $nameErr;?></span>
                 <li>
                     <label>Email <span class="required">*</span></label>
                     <input type="email" name="email" class="field-long" />
+                    <span class="error"> <?php echo $emailErr;?></span>
                 </li>
                 <li>
                     <label> Address <span class="required">*</span></label>
                     <textarea name="add" id="field5" class="field-long field-textarea"></textarea>
+                    <span class="error"> <?php echo $addrErr;?></span>
+                
                 </li>
 
                 <div class="upload-btn-wrapper">
@@ -65,7 +88,9 @@
             </ul>
         </div>
     </form>
-
+    <?php
+    require_once('connect.php');
+?>
     <script src="test.js"></script>
 </body>
 
